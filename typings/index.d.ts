e98c960466508d7a4fce1f70d92934e6fe2332aa
Damo <daiguimao@sightp.com>
2024-04-11 22:18:21 +0800

    perf:optimise json parse

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,4 +1,4 @@
-type Action = { type: '分析代码执行结果' | '解析JSON格式数据' | '拆解目标或代码实现', content: string };
+type Action = { type: '分析代码执行结果' | '重新输出一个JSON格式数据,禁止其他解释' | '拆解目标或代码实现', content: string };
 type ActionRecord = { time: Date, action: Action, result: Result };
 
 type Result = {
